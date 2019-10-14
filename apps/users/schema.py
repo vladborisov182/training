889c7004328ba84e5f32f1c5f469714dd8fc6197
Vlad Borisov <vladborisov182@gmail.com>
2019-10-14 19:13:17 +0300

    filters

--- apps/users/schema.py
+++ apps/users/schema.py
@@ -1,43 +1,29 @@
-import graphene
-from graphene_django.types import DjangoObjectType
+from graphene import ObjectType, relay
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Trainer, User
 
 
-class UserType(DjangoObjectType):
+class UserNode(DjangoObjectType):
     class Meta:
         model = User
+        filter_fields = ["id", "username"]
+        interfaces = (relay.Node,)
 
 
-class TrainerType(DjangoObjectType):
+class TrainerNode(DjangoObjectType):
     class Meta:
         model = Trainer
+        filter_fields = {"name": ["exact", "icontains", "istartswith"]}
+        interfaces = (relay.Node,)
 
 
-class UserQuery(object):
-    user = graphene.Field(UserType, id=graphene.Int(), username=graphene.String())
-    all_users = graphene.List(UserType)
-
-    def resolve_all_users(self, info, **kwargs):
-        return User.objects.all()
-
-    def resolve_user(self, info, **kwargs):
-        identifier = kwargs.get("id")
-        username = kwargs.get("username")
-
-        if identifier:
-            return User.objects.get(pk=identifier)
-        if username:
-            return User.objects.get(username=username)
-        return None
+class UserQuery(ObjectType):
+    user = relay.Node.Field(UserNode)
+    all_users = DjangoFilterConnectionField(UserNode)
 
 
-class TrainerQuery(object):
-    all_trainers = graphene.List(TrainerType)
-    all_trainers_users = graphene.List(UserType)
-
-    def resolve_all_trainers(self, info, **kwargs):
-        return Trainer.objects.all()
-
-    def resolve_all_trainers_users(self, info, **kwargs):
-        return Trainer.objects.select_related("users").all()
+class TrainerQuery(ObjectType):
+    all_trainers = DjangoFilterConnectionField(TrainerNode)
+    all_trainers_users = DjangoFilterConnectionField(UserNode)
--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -4,7 +4,7 @@ import pytest
 from graphene_django.utils.testing import GraphQLTestCase
 
 from apps.users.factories import UserFactory, TrainerFactory
-from apps.users.models import User
+from apps.users.models import User, Trainer
 from conf.schema import schema
 
 pytestmark = pytest.mark.django_db
@@ -25,9 +25,18 @@ class TestModels(GraphQLTestCase):
             """
             query {
                 allTrainers {
-                    name
-                    users {
-                        username
+                    edges {
+                        node {
+                            id,
+                            name
+                            users {
+                                edges {
+                                    node {
+                                        username
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -37,15 +46,15 @@ class TestModels(GraphQLTestCase):
         self.assertResponseNoErrors(response)
 
         content = json.loads(response.content)
-        returned_trainers = content["data"]["allTrainers"]
+        returned_trainers = content["data"]["allTrainers"]["edges"]
         all_trainers_names = [x.name for x in trainers_list]
-        retuned_trainers_names = [x["name"] for x in returned_trainers]
+        retuned_trainers_names = [x["node"]["name"] for x in returned_trainers]
 
         self.assertEqual(set(all_trainers_names), set(retuned_trainers_names))
         self.assertEqual(len(all_trainers_names), len(retuned_trainers_names))
 
         for trainer in returned_trainers:
-            self.assertEqual(len(trainer["users"]), 2)
+            self.assertEqual(len(trainer["node"]["users"]["edges"]), 2)
 
     def test_get_all_users(self):
         users = UserFactory.create_batch(5)
@@ -54,7 +63,11 @@ class TestModels(GraphQLTestCase):
             """
             query {
                 allUsers {
-                    username
+                    edges {
+                        node {
+                            username
+                        }
+                    }
                 }
             }
             """,
@@ -63,9 +76,9 @@ class TestModels(GraphQLTestCase):
         self.assertResponseNoErrors(response)
 
         content = json.loads(response.content)
-        returned_users = content["data"]["allUsers"]
+        returned_users = content["data"]["allUsers"]["edges"]
         all_users_user_names = [x.username for x in users]
-        retuned_users_user_names = [x["username"] for x in returned_users]
+        retuned_users_user_names = [x["node"]["username"] for x in returned_users]
 
         self.assertEqual(set(all_users_user_names), set(retuned_users_user_names))
         self.assertEqual(len(all_users_user_names), len(retuned_users_user_names))
@@ -74,42 +87,78 @@ class TestModels(GraphQLTestCase):
         user = UserFactory()
 
         response = self.query(
-            """
-            query allUsers($id: Int!){
-                user(id: $id) {
-                    id
-                }
-            }
+            f"""
+            query{{
+                allUsers(id: {user.id}) {{
+                    edges {{
+                        node {{
+                            username
+                        }}
+                    }}
+                }}
+            }}
             """,
             op_name="allUsers",
-            variables={"id": user.id},
         )
         self.assertResponseNoErrors(response)
 
         content = json.loads(response.content)
-        returned_user = content["data"]["user"]
+        returned_user = content["data"]["allUsers"]["edges"]
 
         self.assertEqual(len(returned_user), 1)
-        self.assertEqual(int(returned_user["id"]), user.id)
+        self.assertEqual(returned_user[0]["node"]["username"], user.username)
 
     def test_get_user_by_username(self):
         user = UserFactory()
 
         response = self.query(
-            """
-            query allUsers($username: String!){
-                user(username: $username) {
-                    username
-                }
-            }
+            f"""
+            query{{
+                allUsers(username: "{user.username}") {{
+                    edges {{
+                        node {{
+                            username
+                        }}
+                    }}
+                }}
+            }}
             """,
             op_name="allUsers",
-            variables={"username": user.username},
         )
         self.assertResponseNoErrors(response)
 
         content = json.loads(response.content)
-        returned_user = content["data"]["user"]
+        returned_user = content["data"]["allUsers"]["edges"]
 
         self.assertEqual(len(returned_user), 1)
-        self.assertEqual(returned_user["username"], user.username)
+        self.assertEqual(returned_user[0]["node"]["username"], user.username)
+
+    def test_trainers_by_name(self):
+        trainers = TrainerFactory.create_batch(5)
+
+        for trainer in trainers:
+            trainer_name = trainer.name
+            response = self.query(
+                f"""
+                query{{
+                    allTrainers(name_Icontains: "{trainer_name}") {{
+                        edges {{
+                            node {{
+                                name
+                            }}
+                        }}
+                    }}
+                }}
+                """,
+                op_name="allTrainers",
+            )
+            self.assertResponseNoErrors(response)
+
+            content = json.loads(response.content)
+            returned_trainers = content["data"]["allTrainers"]["edges"]
+
+            self.assertEqual(
+                len(returned_trainers),
+                Trainer.objects.filter(name__icontains=trainer_name).count(),
+            )
+            self.assertEqual(returned_trainers[0]["node"]["name"], trainer_name)
